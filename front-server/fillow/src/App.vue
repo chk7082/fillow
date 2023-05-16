1580dc21fcb9474d88df0ef27693f6ae67326fbe
chk7082 <whgusrl7082@gmail.com>
2023-05-16 17:53:46 +0900

    0516 first commit

---
+++ front-server/fillow/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Map</router-link> |
+      <!-- <router-link to="/about">About</router-link> -->
+    </nav>
+    <div class="d-flex">
+      <div class="d-inline-block" style="width: 60%; height: 100%">
+        <router-view/>
+      </div>
+      <div class="d-inline-block" style="width: 40%;">
+        <MovieInfo style="height: 100%"/>
+      </div>
+    </div>
+    
+
+    <!-- <MapView/> -->
+  </div>
+</template>
+
+<script>
+// import MapView from '@/views/MapView.vue'
+import MovieInfo from '@/components/MovieInfo.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // MapView,
+    MovieInfo,
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ front-server/fillow/src/views/MapView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <gmap-map
+        :zoom="3"    
+        :center="center"
+        style="width:100%;  height: 855px;"
+      >
+      <gmap-marker
+        :key="index"
+        v-for="(m, index) in locationMarkers"
+        :position="m.position"
+      ></gmap-marker>
+    </gmap-map>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MapView",
+  data() {
+    return {
+      center: { lat: 37.5642135, lng: 127.0016985 },
+      locationMarkers: [
+        {
+          position: { lat: 37.5642135 ,lng: 127.0016985 },
+        }
+      ],
+      locPlaces: [],
+      existingPlace: null
+    };
+  }, 
+};
+</script>
